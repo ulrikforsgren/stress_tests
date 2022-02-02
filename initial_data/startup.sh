b885cf4284f35fe524fc4ca7506ed4a6257e5916
Ulrik Stridsman <uforsgre@cisco.com>
2022-02-02 15:30:16 +0100

    fetch-ssh-host-keys and sync-from not needed when there are no devices.

--- initial_data/startup.sh
+++ initial_data/startup.sh
@@ -3,10 +3,10 @@
 set -e
 
 ncs_cli -u admin <<EOF
-request devices fetch-ssh-host-keys
-request devices sync-from
-exit
-exit
+#request devices fetch-ssh-host-keys
+#request devices sync-from
+#exit
+#exit
 EOF
 
 
