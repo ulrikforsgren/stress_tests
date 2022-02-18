4328f37b67f6043425051b52eeb5a9ecee7ede5b
Ulrik Stridsman <uforsgre@cisco.com>
2022-02-18 13:31:13 +0100

    Add empty and parateterized tests for template, python and java services.

--- run-stress-tests.sh
+++ run-stress-tests.sh
@@ -17,15 +17,31 @@ export HOST=`hostname`
 
 ./model_a_tests.py --host `echo $NODE | cut -f1 -d:`:8088 crud -n 1000 --json results/model-a-crud-1000-fwslocal.json
 
-#
-# Tests
-#
-
 ./model_a_tests.py --host $NODE crud -n 1000 --json results/model-a-crud-10-local.json
 ./model_a_tests.py --host $NODE clean
 
-./empty_service_tests.py --host $NODE crud -n 1000 --json results/empty-service-crud-10-local.json
-./empty_service_tests.py --host $NODE clean
+#
+# Tests, empty services
+#
 
-./python_service_tests.py --host $NODE crud -n 100 --json results/python-service-crud-10-local.json
+./empty_template_service_tests.py --host $NODE crud -n 1000 --json results/empty-template-service-crud-1000-local.json
+./empty_template_service_tests.py --host $NODE clean
+
+./empty_python_service_tests.py --host $NODE crud -n 1000 --json results/empty-python-service-crud-1000-local.json
+./empty_python_service_tests.py --host $NODE clean
+
+./empty_java_service_tests.py --host $NODE crud -n 1000 --json results/empty-java-service-crud-1000-local.json
+./empty_java_service_tests.py --host $NODE clean
+
+#
+# Tests, services 0.1 s delay
+#
+
+./template_service_tests.py --host $NODE crud -n 1000 --json results/template-service-crud-1000-local.json
+./template_service_tests.py --host $NODE clean
+
+./python_service_tests.py --host $NODE crud -n 1000 --json results/python-service-crud-1000-local.json
 ./python_service_tests.py --host $NODE clean
+
+./java_service_tests.py --host $NODE crud -n 1000 --json results/java-service-crud-1000-local.json
+./java_service_tests.py --host $NODE clean
