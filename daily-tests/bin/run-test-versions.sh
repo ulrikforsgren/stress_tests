6be13fd69a9c2e2d8d05eb911589fefb9e2a04bf
Ulrik Stridsman (uforsgre) <uforsgre@cisco.com>
2022-02-22 08:34:09 +0100

    Fix VERSIONS

--- daily-tests/bin/run-test-versions.sh
+++ daily-tests/bin/run-test-versions.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-VERSIONS=\
-    5.2\
-    5.3\
-    5.4\
-    5.5\
-    5.6\
+VERSIONS="
+    5.2
+    5.3
+    5.4
+    5.5
+    5.6
     5.7
+"
 
 for VER in $VERSIONS; do
   (mkdir $VER; cd $VER; ~/daily-tests/bin/run-test-version.sh $VER build | tee run-test-version.log)
