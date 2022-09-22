a8b8cd9c51373c0f25430a8a7efe6784646b8616
Ulrik Stridsman <uforsgre@cisco.com>
2022-09-22 07:47:06 +0200

    Update delay tests.

--- run-delay-stress-tests.sh
+++ run-delay-stress-tests.sh
@@ -19,35 +19,22 @@ export HOST=`hostname`
 ./python_service_tests.py --host $NODE crud -n 1000 -p delay=0 --json results/python-service-crud-1000-delay-0-local.json
 ./python_service_tests.py --host $NODE clean
 
-./java_service_tests.py --host $NODE crud -n 1000 -p delay=0 --json results/java-service-crud-1000-delay-0-local.json
-./java_service_tests.py --host $NODE clean
-
 #
 # Tests, services 0.1 s delay
 #
 
-./python_service_tests.py --host $NODE crud -n 1000 -p delay=0 --json results/python-service-crud-1000-delay-0.1-local.json
+./python_service_tests.py --host $NODE crud -n 1000 -p delay=100 --json results/python-service-crud-1000-delay-0.1-local.json
 ./python_service_tests.py --host $NODE clean
 
-./java_service_tests.py --host $NODE crud -n 1000 -p delay=0 --json results/java-service-crud-1000-delay-0.1-local.json
-./java_service_tests.py --host $NODE clean
-
-#
 # Tests, services 0.5 delay
 #
 
-./python_service_tests.py --host $NODE crud -n 1000 -p delay=0 --json results/python-service-crud-1000-delay-0.5-local.json
+./python_service_tests.py --host $NODE crud -n 1000 -p delay=500 --json results/python-service-crud-1000-delay-0.5-local.json
 ./python_service_tests.py --host $NODE clean
 
-./java_service_tests.py --host $NODE crud -n 1000 -p delay=0 --json results/java-service-crud-1000-delay-0.5-local.json
-./java_service_tests.py --host $NODE clean
-
-#
 # Tests, services 2.0 delay
 #
 
-./python_service_tests.py --host $NODE crud -n 1000 -p delay=0 --json results/python-service-crud-1000-delay-2.0-local.json
+./python_service_tests.py --host $NODE crud -n 1000 -p delay=2000 --json results/python-service-crud-1000-delay-2.0-local.json
 ./python_service_tests.py --host $NODE clean
 
-./java_service_tests.py --host $NODE crud -n 1000 -p delay=0 --json results/java-service-crud-1000-delay-2.0-local.json
-./java_service_tests.py --host $NODE clean
