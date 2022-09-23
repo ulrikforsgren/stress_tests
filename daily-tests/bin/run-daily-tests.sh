099454f8ba0a9709f6f43ebf43946c91c016e43a
Ulrik Stridsman <uforsgre@cisco.com>
2022-09-23 10:00:49 +0200

    Clean up process before starting daily-tests.

--- daily-tests/bin/run-daily-tests.sh
+++ daily-tests/bin/run-daily-tests.sh
@@ -3,6 +3,11 @@
 export DATE=`date +%Y%m%d-%H%M`
 export DAILYDIR=~/daily-tests/$DATE
 
+#
+# Preventive clean up
+#
+pkill -f ncs.smp
+pkill -f reference_server.py
 
 mkdir -p $DAILYDIR
 cd $DAILYDIR
